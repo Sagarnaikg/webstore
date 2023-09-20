68a4a81afcc57e707d7ae9153496969006db7ba5
sagar naik <sagarnaikg00@gmail.xom>
2023-09-20 23:50:37 +0530

    final code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { items } from './data'
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper' >
+      <div className='page' >
+        <div className='products' >
+          {items.map(item => {
+            return <div className='product' >
+              <div className='image' ><img src={item.image} /> </div>
+              <div className='info' >
+                <div className='name' >{item.name}</div>
+                <div className='price' ><span>{item.offer}%</span> @<span className='amount' > {item.price} INR</span></div>
+                <div className='link' ><a href={item.link}>Click Here</a></div>
+              </div>
+            </div>
+          })
+          }</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/data.js
@@ -0,0 +1,35 @@
+import Product1 from '../src/images/S21 fe.jpg'
+import Product2 from '../src/images/14pro display picture.JPEG'
+import Product3 from '../src/images/15 pro white titanium.PNG'
+import Product4 from '../src/images/no-image.jpeg'
+
+export const items = [
+    {
+        name: "product 1",
+        image: Product1,
+        offer: 10,
+        price: 98_000,
+        link: "https://affiliate-program.amazon.in/home/textlink/sitestripe/ref=amb_link_7-PRmoZsPVG-u-VLYAceOw_1?pf_rd_p=4dca2296-2f93-4dce-88db-a64bce7befd9"
+    },
+    {
+        name: "product 2",
+        image: Product2,
+        offer: 10,
+        price: 98_000,
+        link: "https://affiliate-program.amazon.in/home/textlink/sitestripe/ref=amb_link_7-PRmoZsPVG-u-VLYAceOw_1?pf_rd_p=4dca2296-2f93-4dce-88db-a64bce7befd9"
+    },
+    {
+        name: "product 3",
+        image: Product3,
+        offer: 10,
+        price: 98_000,
+        link: "https://affiliate-program.amazon.in/home/textlink/sitestripe/ref=amb_link_7-PRmoZsPVG-u-VLYAceOw_1?pf_rd_p=4dca2296-2f93-4dce-88db-a64bce7befd9"
+    },
+    {
+        name: "product 4",
+        image: Product4,
+        offer: 10,
+        price: 98_000,
+        link: "https://affiliate-program.amazon.in/home/textlink/sitestripe/ref=amb_link_7-PRmoZsPVG-u-VLYAceOw_1?pf_rd_p=4dca2296-2f93-4dce-88db-a64bce7befd9"
+    }
+]
